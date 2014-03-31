27a62fc0c9fe67f6263018b01263fab6f9d97bde
JustWalters <jw3043@columbia.edu>
2014-03-31 16:38:14 -0400

    Update game_manager.js
    Put value back in constructor

--- js/game_manager.js
+++ js/game_manager.js
@@ -5,6 +5,7 @@ function GameManager(size, InputManager, Actuator, StorageManager) {
   this.actuator       = new Actuator;
 
   this.startTiles     = 2;
+  this.totalValue     = 0;
   
   this.inputManager.on("move", this.move.bind(this));
   this.inputManager.on("restart", this.restart.bind(this));
