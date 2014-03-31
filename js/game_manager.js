3c377785bcefe642aa3cf47a3a74848e9bb93f25
JustWalters <jw3043@columbia.edu>
2014-03-31 16:40:58 -0400

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -77,7 +77,7 @@ GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     
     //TODO figure out good formula for mod
-    var mod = log10(this.totalValue) + 1;
+    var mod = this.totalValue);
     console.log(this.totalValue);
     console.log(mod);
     
