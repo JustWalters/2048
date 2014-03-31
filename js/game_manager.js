87cd607f1949c5d2d368fdc3d228e17bd4ebb424
JustWalters <jw3043@columbia.edu>
2014-03-31 01:49:40 -0400

    Update game_manager.js
    Added if.tile

--- js/game_manager.js
+++ js/game_manager.js
@@ -82,8 +82,10 @@ GameManager.prototype.addRandomTile = function () {
         for (var y = 0; y < this.size; y++) {
           tile = this.grid.cellContent({ x: x, y: y });
           
+          if (tile){
           console.log(tile);
           console.log(tile.value);
+          }
         }
       }
 
